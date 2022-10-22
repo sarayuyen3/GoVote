d5b6776ab1728eeba5c10dffb193758a4d8f9886
Sarayu Yenumula <sarayu@ad.unc.edu>
2022-10-22 19:52:56 -0400

    Initial Commit

---
+++ GoVote/GoVoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoVoteApp.swift
+//  GoVote
+//
+//  Created by Sarayu Yenumula on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GoVoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
